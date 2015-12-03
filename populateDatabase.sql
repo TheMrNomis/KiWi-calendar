3792005a2af683b29cf338c11148466b856c8b9a
TheMrNomis <n0m1s@homnomnom.fr>
2015-12-03 11:31:19 +0100

    populating database made easy

---
+++ populateDatabase.sql
@@ -0,0 +1,16 @@
+INSERT INTO sous_categorie(sous_cat_id, sous_cat_tab, sous_cat_titre) VALUES(0,0, "Conférences, évènements");
+INSERT INTO sous_categorie(sous_cat_id, sous_cat_tab, sous_cat_titre) VALUES(1,0, "Clubs, Associations");
+
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("[IN] Imagerie numérique", 0);
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("[IS] Ingénierie pour la santé", 0);
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("[MAT] Matériaux", 0);
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("[SI] Systèmes d'information", 0);
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("[T&R] Télécoms et réseaux", 0);
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("[TICB] Domotique", 0);
+
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("Isati", 1);
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("Club Dorothée", 1);
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("Club de golf", 1);
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("Club Maté", 1);
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("Club Beurre Salé", 1);
+INSERT INTO categorie(cat_titre, sous_cat_id) VALUES("Association de défence des kakapos", 1);
