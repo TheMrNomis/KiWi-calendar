c0024459d87f31121ec0b526241fa505c4867c4e
TheMrNomis <n0m1s@homnomnom.fr>
2015-12-10 10:49:29 +0100

    moved day and months arrays to a separate file

---
+++ datetimeOperations.php
@@ -0,0 +1,36 @@
+<?php
+$days = array(
+    1 => htmlentities("Lun"),
+    2 => htmlentities("Mar"),
+    3 => htmlentities("Mer"),
+    4 => htmlentities("Jeu"),
+    5 => htmlentities("Ven"),
+    6 => htmlentities("Sam"),
+    7 => htmlentities("Dim")
+);
+
+$daysFull = array(
+    1 => htmlentities("Lundi"),
+    2 => htmlentities("Mardi"),
+    3 => htmlentities("Mercredi"),
+    4 => htmlentities("Jeudi"),
+    5 => htmlentities("Vendredi"),
+    6 => htmlentities("Samedi"),
+    7 => htmlentities("Dimamche")
+);
+
+$months = array(
+    1 => htmlentities("Janvier"),
+    2 => htmlentities("Fevrier"),
+    3 => htmlentities("Mars"),
+    4 => htmlentities("Avril"),
+    5 => htmlentities("Mai"),
+    6 => htmlentities("Juin"),
+    7 => htmlentities("Juillet"),
+    8 => htmlentities("Août"),
+    9 => htmlentities("Septembre"),
+    10 => htmlentities("Octobre"),
+    11 => htmlentities("Novembre"),
+    12 => htmlentities("Décembre")
+);
+?>
